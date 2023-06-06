0454c835dc920637f289482f460232c4d185590c
dannyadkins <danielpadk@gmail.com>
2023-06-06 04:39:30 +0000

    refactor more

---
+++ data_parallelism.py
@@ -0,0 +1,49 @@
+# This is the most naive iteration.
+# Next, do it with Horovod and then with PyTorch DDP.
+# Also, try to do it with some other strategies: async gradients, model parallel, tensor parallel, pipeline parallel, etc.
+# Also, try proper sharded dataloader, memmap... 
+# Also try ring-allreduce, hierarchical allreduce, etc.
+
+from mpi4py import MPI
+import torch 
+import torch.nn as nn
+from model import CNN 
+from utils import init_mpi, init_model, get_full_dataset, get_shard, plot_losses, fwd, train_model
+
+comm, rank, size, device = init_mpi()
+
+torch.manual_seed(0)
+
+model = init_model(comm, rank, device)
+optim = torch.optim.Adam(model.parameters(), lr=0.001)
+loss_fn = nn.CrossEntropyLoss()
+
+full_dataset = get_full_dataset()
+images, labels = get_shard(full_dataset, rank, size)
+images = images.to(device)
+labels = labels.to(device)
+
+training_curve = []
+
+# train the model for 10 epochs
+batch_size = 256
+num_epochs = 5
+
+def update_func(comm, rank, size, device, model, optim):
+    for param in model.parameters():
+        grad_numpy = param.grad.data.cpu().numpy()  # convert tensor to numpy array
+        comm.Allreduce(MPI.IN_PLACE, grad_numpy, op=MPI.SUM)  # in-place sum of gradients across all GPUs
+        avg_grad = torch.from_numpy(grad_numpy / size).to(device)  # convert back to tensor and average
+        # update with the average gradient
+        param.grad.data = avg_grad
+    
+    # update the model parameters
+    optim.step()
+
+train_model(comm, rank, size, device, model, optim, loss_fn, images, labels, update_func=update_func, num_epochs=10)
+
+# plot the training curve
+plot_losses(training_curve, "dp_rank{0}_size{1}.png".format(rank,size))
+
+if rank == 0:
+    torch.save(model.state_dict(), "model.pt")
--- utils.py
+++ utils.py
@@ -1,9 +1,19 @@
 import torch 
 from model import CNN 
+import matplotlib.pyplot as plt 
+from mpi4py import MPI
+
+def init_mpi():
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+
+    device = torch.device("cuda:{0}".format(rank))
+    return comm, rank, size, device
 
 # instantiate the model; broadcast to other ranks if rank == 0
 # if rank > 0, receive the model from rank 0
-def instantiate_model(comm, rank, device):
+def init_model(comm, rank, device):
     if rank == 0:
         model = CNN().to(device)
         comm.bcast(model, root=0)
@@ -65,3 +75,43 @@ def get_shard(data, rank, size):
     images = images[start:end]
     labels = labels[start:end]
     return images, labels
+
+def plot_losses(losses, name):
+    plt.plot(losses)
+    plt.xlabel("Iteration")
+    plt.ylabel("Loss")
+    # save as name.png
+    plt.savefig(name + ".png")
+    plt.close()
+
+# some function to do a pass without updating the model
+def fwd(model, optim, loss_fn, images, labels):
+    # zero the gradients
+    optim.zero_grad()
+    # forward pass
+    output = model(images)
+    # compute loss
+    loss = loss_fn(output, labels)
+    # backward pass
+    return loss
+
+def train_model(comm, rank, size, device, model, optim, loss_fn, images, labels, update_func, num_epochs=1, batch_size=256, training_curve=[], show_logs=False):
+    for epoch in range(num_epochs):
+        for i in range(len(images)//batch_size):
+            mini = slice(i*batch_size, (i+1)*batch_size)
+
+            loss = fwd(model, optim, loss_fn, images[mini], labels[mini])
+            loss.backward()
+
+            print("Rank {0} epoch {1} loss: {2}".format(rank, epoch, loss.item()))
+            # calculate the gradients and allreduce 
+            update_func(comm, rank, size, device, model, optim)
+
+            training_curve.append(loss.item())
+
+    # print the first few model parameters to make sure they're the same
+    if (show_logs):
+        print("Rank {0} model parameters:".format(rank))
+        for param in model.parameters():
+            print(param[0][0][0][0])
+            break
